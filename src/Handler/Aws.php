9e27202e7830d1e491168e0bbccf9dec1c828c31
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-23 18:04:11 +0100

    add aws push implementation using bref

---
+++ src/Handler/Aws.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Adapter\Aws\Handler;
+
+use Bref\Context\Context;
+use Bref\Event\Http\HttpHandler;
+use Bref\Event\Http\HttpRequestEvent;
+use Bref\Event\Http\HttpResponse;
+use Fusio\Engine\Serverless\ExecutorInterface;
+
+/**
+ * Aws
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class Aws extends HttpHandler
+{
+    /**
+     * @var ExecutorInterface
+     */
+    private $executor;
+
+    /**
+     * @var int
+     */
+    private $routeId;
+
+    public function __construct(ExecutorInterface $executor, int $routeId)
+    {
+        $this->executor = $executor;
+        $this->routeId = $routeId;
+    }
+
+    public function handleRequest(HttpRequestEvent $event, Context $context): HttpResponse
+    {
+        $uriParameters = []; // @TODO get uri path parameters
+
+        $response = $this->executor->execute(
+            $this->routeId,
+            $event->getMethod(),
+            $event->getUri(),
+            $uriParameters,
+            $event->getHeaders(),
+            $event->getBody()
+        );
+
+        return new HttpResponse((string) $response->getBody(), $response->getHeaders(), $response->getStatusCode());
+    }
+}
