5c35385c9ccfe3faa3ae1200275d30409afc3229
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-04-02 12:11:00 +0200

    fix namespace

--- src/Routes/AwsLambda.php
+++ src/Routes/AwsLambda.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Adapter\Sql\Routes;
+namespace Fusio\Adapter\Aws\Routes;
 
 use Aws\Sdk;
 use Fusio\Adapter\Aws\Action\AwsLambdaInvoke;
